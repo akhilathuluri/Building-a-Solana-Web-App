aa18e8bace8e0978573a5dc47707dbba29302eef
Akhil Athuluri <8309889800a@gmail.com>
2023-04-09 16:18:02 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import AddressForm from '../components/AddressForm'
+import * as Web3 from '@solana/web3.js'
+
+const Home: NextPage = () => {
+  const [balance, setBalance] = useState(0)
+  const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false);
+
+  const addressSubmittedHandler = (address: string) => {
+    try {
+      setAddress(address)
+      const key = new Web3.PublicKey(address)
+      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
+
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
+      })
+    } catch (error) {
+      setAddress('')
+      setBalance(0)
+      alert(error)
+    }
+  }
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <p>
+          Start Your Solana Journey
+        </p>
+        <AddressForm handler={addressSubmittedHandler} />
+        <p>{`Address: ${address}`}</p>
+        <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? 'Yep' : 'Nope'}`}</p>
+      </header>
+    </div>
+  )
+}
+
+export default Home
